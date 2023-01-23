21aee188ab448a5ed2d79df5f93a675735d2918a
chendaozhang <1205308740@qq.com>
2023-01-23 16:49:25 +0800

    计算需要的page宽度

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import s from '@style/App.module.scss'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+  }, [])
   return (
     <div>
       <div className={s.wrapper}></div>
