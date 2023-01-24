eb29aed71b60b5f30d2fef762712491308619047
chendaozhang <1205308740@qq.com>
2023-01-24 19:32:47 +0800

    完成tableChart

--- src/helpers/autoSetInterval.ts
+++ src/helpers/autoSetInterval.ts
@@ -8,7 +8,7 @@ export const autoSetInterval = (refTbody: HTMLTableSectionElement, refStartHeigh
             const y = setInterval(() => {
                 refTbody?.scrollTo({ top: refStartHeight - 1 })
                 refStartHeight -= 1
-                if (refStartHeight <= 0) {
+                if (refStartHeight <= -30) {
                     autoSetInterval(refTbody, refStartHeight, refHeight)
                     clearInterval(y)
                 }
