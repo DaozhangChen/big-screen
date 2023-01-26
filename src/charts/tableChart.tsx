534205624b99b18301bb99fbf0fdec88bd28625d
chendaozhang <1205308740@qq.com>
2023-01-26 20:23:23 +0800

    完成tableChart

--- src/charts/tableChart.tsx
+++ src/charts/tableChart.tsx
@@ -21,7 +21,7 @@ const TableChart = () => {
                     <thead>
                         <tr>
                             <th>地区</th>
-                            <th>人口</th>
+                            <th>人口（千人）</th>
                             <th>比例</th>
                         </tr>
                     </thead>
@@ -31,47 +31,47 @@ const TableChart = () => {
                         <tr>
                             <td>福州市</td>
                             <td>8500</td>
-                            <td>321</td>
+                            <td>20.36%</td>
                         </tr>
                         <tr>
                             <td>厦门市</td>
                             <td>5164</td>
-                            <td>321</td>
+                            <td>12.37%</td>
                         </tr>
                         <tr>
                             <td>泉州市</td>
                             <td>8782</td>
-                            <td>321</td>
+                            <td>21.04%</td>
                         </tr>
                         <tr>
                             <td>漳州市</td>
                             <td>5054</td>
-                            <td>321</td>
+                            <td>12.10%</td>
                         </tr>
                         <tr>
                             <td>宁德市</td>
                             <td>3146</td>
-                            <td>321</td>
+                            <td>7.54%</td>
                         </tr>
                         <tr>
                             <td>南平市</td>
                             <td>2680</td>
-                            <td>321</td>
+                            <td>6.41%</td>
                         </tr>
                         <tr>
                             <td>三明市</td>
                             <td>2486</td>
-                            <td>321</td>
+                            <td>5.96%</td>
                         </tr>
                         <tr>
                             <td>龙岩市</td>
                             <td>2723</td>
-                            <td>321</td>
+                            <td>6.52%</td>
                         </tr>
                         <tr>
                             <td>莆田市</td>
                             <td>3210</td>
-                            <td>321</td>
+                            <td>7.68%</td>
                         </tr>
                     </tbody>
                 </table>
